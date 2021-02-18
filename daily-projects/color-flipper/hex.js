8bb78e46182aa8bd54c758dfcf377798cf7ee667
Roman Turner <rn.turner@live.com>
2021-02-17 17:13:32 -0800

    project page complete' git push

--- daily-projects/color-flipper/hex.js
+++ daily-projects/color-flipper/hex.js
@@ -4,8 +4,9 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
-    let hexColor = '#'
-    for (let i = 0; i < 6; i++){
+    let hexColor = "#";
+  for (let i = 0; i < 6; i++){
+     
         hexColor += hex[getRandomNumber()];
     }
     color.textContent = hexColor;
